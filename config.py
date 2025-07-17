ebf567d607abc6815aa1fdc8a7ab9fac66fc1def
Arrobear <103542487+Arrobear@users.noreply.github.com>
2025-07-17 16:07:41 +0800

    11

--- config.py
+++ config.py
@@ -7,12 +7,18 @@ import itertools
 import os
 
 # 参数设置
-checkpoint = "bigcode/starcoder2-15b"
+#checkpoint = "bigcode/starcoder2-15b"
 # checkpoint = "bigcode/starcoder2-3b"
-model_path = "/nasdata/haoyahui/Model/starcoder2-15b"
+# model_path = "/nasdata/haoyahui/Model/starcoder2-15b"
+model_path = "/nasdata/haoyahui/Model/codellama-13b"
+# model_path = "/nasdata/haoyahui/Model/deepseek-coder-6.7b-base"
+# model_path = "/nasdata/haoyahui/Model/CodeLlama-70b-Instruct-hf"
+# model_path = "/nasdata/haoyahui/Model/DeepSeek-R1-Distill-Qwen-32B"
+# model_path = "/nasdata/haoyahui/Model/Meta-Llama-3-70B-Instruct"
+
 
 torch.cuda.set_device(0)  # 指定默认使用 GPU 0
 
-device = torch.device("cuda:7") 
+device = torch.device("cuda:0") 
 
 lib_name = "torch"  # 库名称
--- test.py
+++ test.py
@@ -4,3 +4,10 @@ from main import *
 from generate_prompt import *
 
 
+
+
+# 尝试加载 tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+# 尝试加载模型
+model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
