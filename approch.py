108f7da3c532d8549041d57c1a0cd8bce722ac24
Arrobear <103542487+Arrobear@users.noreply.github.com>
2025-07-17 16:58:37 +0800

    1

--- approch.py
+++ approch.py
@@ -14,8 +14,7 @@ def generate_api_conditions(api_names):
     #加载模型
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
-    # to use Multiple GPUs do `model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto")`
-    model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
+    model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
     i = 0
 
     while(True):
