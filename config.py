bd83c267909c8aad0e3a05ee20ebb1fa4bc443fe
Arrobear <103542487+Arrobear@users.noreply.github.com>
2025-07-21 13:31:13 +0800

    11

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ import bitsandbytes
 # 参数设置
 #checkpoint = "bigcode/starcoder2-15b"
 # checkpoint = "bigcode/starcoder2-3b"
-# model_path = "/nasdata/haoyahui/Model/starcoder2-15b"
+model_path = "/nasdata/haoyahui/Model/starcoder2-15b"
 # model_path = "/nasdata/haoyahui/Model/codellama-13b"
 # model_path = "/nasdata/haoyahui/Model/deepseek-coder-6.7b-base"
 # model_path = "/nasdata/haoyahui/Model/CodeLlama-70b-Instruct-hf"
-model_path = "/nasdata/haoyahui/Model/DeepSeek-R1-Distill-Qwen-32B"
+# model_path = "/nasdata/haoyahui/Model/DeepSeek-R1-Distill-Qwen-32B"
 # model_path = "/nasdata/haoyahui/Model/Meta-Llama-3-70B-Instruct"
 
 
