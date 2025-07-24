913daec57e3422a7c8b3b4fa754510e9502d3f18
Arrobear <103542487+Arrobear@users.noreply.github.com>
2025-07-25 06:06:24 +0800

    1

--- approch.py
+++ approch.py
@@ -15,7 +15,7 @@ def generate_api_conditions(api_names):
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     # model = AutoModelForCausalLM.from_pretrained(model_path, load_in_8bit=True,device_map={"": 0} )
-    model = AutoModelForCausalLM.from_pretrained(model_path,device_map={"": 0} )
+    model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype = torch.float16, device_map={"": 0} )
     #,  torch_dtype = torch.float16
     i = 0
 
